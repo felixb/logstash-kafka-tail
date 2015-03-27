a693635e94d4f38eb1c46d2ecf6eb9de50539577
Felix Bechstein <f@ub0r.de>
2015-03-27 17:24:44 +0100

    add some more tests

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ get:
 	go get -v github.com/Shopify/sarama
 	go get -v github.com/docker/docker/pkg/mflag
 	go get -v github.com/stretchr/testify/assert
+	go get -v github.com/stretchr/testify/mock
 
 build: test
 	go build
--- filter_test.go
+++ filter_test.go
@@ -4,7 +4,32 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type MockedPrinter struct {
+	mock.Mock
+}
+
+func (mck *MockedPrinter) Print(m *Message) {
+	mck.Called()
+}
+
+func TestFilterPrint(t *testing.T) {
+	p := new(MockedPrinter)
+	p.On("Print").Return()
+
+	filters := map[string]string{"a": "1"}
+	f := NewFilter(filters, p)
+
+	m := Message{"b": "2"}
+	f.Print(&m)
+	p.AssertNotCalled(t, "Print")
+
+	m = Message{"a": "1"}
+	f.Print(&m)
+	p.AssertCalled(t, "Print")
+}
 
 func TestFilterFilter(t *testing.T) {
 	filters := map[string]string{"a": "abc", "b": "1", "c": "true"}
